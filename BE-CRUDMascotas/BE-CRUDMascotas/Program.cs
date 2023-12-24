70e0dca0b9c0476c40cd10a621d02d06d12ef4b8
Jirgort <Jirgort@gmail.com>
2023-12-24 02:08:35 -0600

    Creacion del modelo, contexto y primera migracion
    En este caso se creo el modelo y se hiceiron ajustes en el appsetting.json, program.
    Comandos:
    Add-Migration initial
    Update database

--- BE-CRUDMascotas/BE-CRUDMascotas/Program.cs
+++ BE-CRUDMascotas/BE-CRUDMascotas/Program.cs
@@ -1,3 +1,5 @@
+using BE_CRUDMascotas.Models;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//add context
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conexion"));
+});
 
 var app = builder.Build();
 
